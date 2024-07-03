67f39669a4928c433ce5f9491a7665dd553808ef
hiepnt <thaihiep232002@gmail.com>
2024-07-04 00:52:50 +0700

    feat: fix bugs build 222

--- build_files.sh
+++ build_files.sh
@@ -1,18 +1,7 @@
-echo "BUILD START"
+#!/bin/bash
 
-# create a virtual environment named 'venv' if it doesn't already exist
-python3.9 -m venv venv
-
-# activate the virtual environment
-source venv/bin/activate
-
-# install all deps in the venv
+# Install dependencies
 pip install -r requirements.txt
 
-# collect static files using the Python interpreter from venv
+# Collect static files
 python manage.py collectstatic --noinput
-
-echo "BUILD END"
-
-# [optional] Start the application here 
-# python manage.py runserver
