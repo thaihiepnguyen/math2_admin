99684af0e207ac1e93f978026a1134d8a9d8ab0e
hiepnt <thaihiep232002@gmail.com>
2024-07-04 02:17:31 +0700

    feat: re store

--- math2/settings.py
+++ math2/settings.py
@@ -124,8 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
