760780eda24f88084951f732603f024cb88b7619
hiepnt <thaihiep232002@gmail.com>
2024-07-04 01:06:54 +0700

    feat: fix

--- math2/settings.py
+++ math2/settings.py
@@ -131,6 +131,7 @@ STATICFILES_DIRS = (
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
